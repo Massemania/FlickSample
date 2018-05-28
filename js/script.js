d3b7ac854d8a762f790ca50631d7fe6088a20918
mats-magnusson <mm223jz@student.lnu.se>
2018-05-28 09:30:52 +0200

    Small fix

--- js/script.js
+++ js/script.js
@@ -11,9 +11,9 @@
  * Funktion removeChild som kollar om det finns något i elementet och om det finns tar bort det.
  * @param {*} value
  */
-function removeChild (value) {
-  while (value.firstChild) {
-      value.removeChild(value.firstChild)
+function removeChild(value) {
+    while (value.firstChild) {
+        value.removeChild(value.firstChild)
     }
 }
 
@@ -26,36 +26,36 @@ let form = document.querySelector('#sbx')
 
 form.addEventListener('submit', event => {
         // console.log('Saving value', form.elements.search.value)
-  let search = form.elements.search.value
-  removeChild(divResults)
-  let div = document.createElement('div')
-  div.setAttribute('id', 'results')
-  divResults.appendChild(div)
-  let p = document.createElement('p')
-  let t = document.createTextNode('Click on a picture to add to your gallery')
-  p.appendChild(t)
-  div.appendChild(p)
-  flickrApi(search).then((images) => {
-          images.map((img) => {
-              let a = document.createElement('a')
-              a.setAttribute('target', '_blank')
-              a.href = img.src
-              let b = img.appendChild(a)
-              div.appendChild(img)
-              divResults.scrollIntoView()
+        let search = form.elements.search.value
+        removeChild(divResults)
+        let div = document.createElement('div')
+        div.setAttribute('id', 'results')
+        divResults.appendChild(div)
+        let p = document.createElement('p')
+        let t = document.createTextNode('Click on a picture to add to your gallery')
+        p.appendChild(t)
+        div.appendChild(p)
+        flickrApi(search).then((images) => {
+            images.map((img) => {
+                let a = document.createElement('a')
+                a.setAttribute('target', '_blank')
+                a.href = img.src
+                let b = img.appendChild(a)
+                div.appendChild(img)
+                divResults.scrollIntoView()
             })
         })
-  event.preventDefault()
-})
+        event.preventDefault()
+    })
     /**
      * Funktion handleErrors som tar hand om eventuella errors och kastar ett error meddelande
      * @param {*} response
      */
-function handleErrors (response) {
-  if (!response.ok) {
-      throw Error(response.statusText)
+function handleErrors(response) {
+    if (!response.ok) {
+        throw Error(response.statusText)
     }
-  return response
+    return response
 }
 
 /**
@@ -64,22 +64,22 @@ function handleErrors (response) {
  * @param {*} tags
  */
 let flickrApi = (tags) => {
-  let apiUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d7bd4ee1b1cf2f0882e2c93021e51721&tags=${tags}&format=json&nojsoncallback=1`
-  return fetch(apiUrl)
+    let apiUrl = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=d7bd4ee1b1cf2f0882e2c93021e51721&tags=${tags}&format=json&nojsoncallback=1`
+    return fetch(apiUrl)
         .then(handleErrors)
         .then((resp) => resp.json())
         .then((data) => {
-          let images = []
-          data.photos.photo.map((i, index) => {
+            let images = []
+            data.photos.photo.map((i, index) => {
                 // console.log(i.farm)
-              let url = 'http://farm' + i.farm + '.static.flickr.com/' + i.server + '/' + i.id + '_' + i.secret + '.jpg'
-              let img = document.createElement('img')
-              img.setAttribute('id', index)
-              img.setAttribute('alt', i.title)
-              img.src = url
-              images.push(img)
+                let url = 'http://farm' + i.farm + '.static.flickr.com/' + i.server + '/' + i.id + '_' + i.secret + '.jpg'
+                let img = document.createElement('img')
+                img.setAttribute('id', index)
+                img.setAttribute('alt', i.title)
+                img.src = url
+                images.push(img)
             })
-          return images
+            return images
         })
 }
 
@@ -93,10 +93,10 @@ let gallery = localStorage.getItem('links') ? JSON.parse(localStorage.getItem('l
  * Skapar en lyssnare som om man klickar på en bild efter sökning på flickr lagras i local storage
  */
 // let sel = document.querySelector('#results')
-divResults.addEventListener('click', function (e) {
-  let pic = e.target.getAttribute('src')
-  gallery.push(pic)
-  localStorage.setItem('links', JSON.stringify(gallery))
+divResults.addEventListener('click', (e) => {
+    let pic = e.target.getAttribute('src')
+    gallery.push(pic)
+    localStorage.setItem('links', JSON.stringify(gallery))
         // console.log('Added pic:', e.target.getAttribute('src'))
 })
 
@@ -105,9 +105,9 @@ divResults.addEventListener('click', function (e) {
  */
 let selGallery = document.querySelector('#gallery')
 let galleryImg = gallery.map((url) => {
-  let img = document.createElement('img')
-  img.src = url
-  selGallery.appendChild(img)
+    let img = document.createElement('img')
+    img.src = url
+    selGallery.appendChild(img)
 })
 
 /**
@@ -116,17 +116,17 @@ let galleryImg = gallery.map((url) => {
 let textGallery = document.querySelector('#text')
 let large = document.querySelector('#large')
 selGallery.addEventListener('click', (e) => {
-  removeChild(large)
-  let img = document.createElement('img')
-  let url = e.target.getAttribute('src')
-  img.src = url
-  large.appendChild(img)
-  removeChild(textGallery)
-  let p = document.createElement('p')
-  let t = document.createTextNode('Click on large image to remove')
-  p.appendChild(t)
-  textGallery.appendChild(p)
-  large.scrollIntoView()
+    removeChild(large)
+    let img = document.createElement('img')
+    let url = e.target.getAttribute('src')
+    img.src = url
+    large.appendChild(img)
+    removeChild(textGallery)
+    let p = document.createElement('p')
+    let t = document.createTextNode('Click on large image to remove')
+    p.appendChild(t)
+    textGallery.appendChild(p)
+    large.scrollIntoView()
 })
 
 /**
@@ -134,11 +134,11 @@ selGallery.addEventListener('click', (e) => {
  */
 let viewGalleryButton = document.querySelector('#viewGalleryButton')
 viewGalleryButton.addEventListener('click', (e) => {
-  if (selGallery.style.display === 'none') {
-      selGallery.style.display = 'block'
-      selGallery.scrollIntoView()
+    if (selGallery.style.display === 'none') {
+        selGallery.style.display = 'block'
+        selGallery.scrollIntoView()
     } else {
-      selGallery.style.display = 'none'
+        selGallery.style.display = 'none'
     }
 })
 
@@ -146,11 +146,11 @@ viewGalleryButton.addEventListener('click', (e) => {
  * Lyssnare som tar bort den större bilden om man klickar på den
  */
 large.addEventListener('click', () => {
-  let item = document.querySelector('#large img')
+    let item = document.querySelector('#large img')
         // console.log('Remove item: ', item)
-  item.remove()
+    item.remove()
         // textGallery.remove()
-  removeChild(textGallery)
+    removeChild(textGallery)
 })
 
 /**
@@ -158,5 +158,5 @@ large.addEventListener('click', () => {
  */
 let emptyGallery = document.querySelector('#removeButton')
 emptyGallery.addEventListener('click', (e) => {
-  localStorage.clear()
+    localStorage.clear()
 })
